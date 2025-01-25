832505b0ebc9be536b8dec6d21362bbd3a4f66c3
Corey Pierce <coreypierce11@gmail.com>
2025-01-25 11:13:05 -0500

    Change joystick to gamepad and print stick axis to console

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-static SDL_Joystick *joystick = NULL;
+//static SDL_Joystick *joystick = NULL;
+static SDL_Gamepad *gamepad = NULL;
 static SDL_Color colors[64];
 
 int fpsCap = 60;
@@ -27,6 +28,7 @@ int initEverything();
 int handleEvents();
 void simulate();
 void render();
+void print_joysticks();
 
 int main(int argc, char* argv[]) {
     if(initEverything() != SDL_APP_CONTINUE){
@@ -41,11 +43,27 @@ int main(int argc, char* argv[]) {
     while(running){
         frameStart = SDL_GetTicks();
         startTime = SDL_GetPerformanceCounter();
-        
-        int result = handleEvents();
-        if(result == SDL_APP_SUCCESS){
-            running = 0;
+
+        // input handling (move into function)
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            if (event.type == SDL_EVENT_QUIT) {
+                running = 0;  /* end the program, reporting success to the OS. */
+            } else if (event.type == SDL_EVENT_GAMEPAD_ADDED) {
+                /* this event is sent for each hotplugged stick, but also each already-connected joystick during SDL_Init(). */
+                if (gamepad == NULL) {  /* we don't have a stick yet and one was added, open it! */
+                    gamepad = SDL_OpenGamepad(event.gdevice.which);
+                }
+            } else if (event.type == SDL_EVENT_GAMEPAD_REMOVED) { // from example
+                if (gamepad && (SDL_GetGamepadID(gamepad) == event.gdevice.which)) {
+                    SDL_CloseGamepad(gamepad);  /* our joystick was unplugged. */
+                    gamepad = NULL;
+                }
+            }
         }
+        print_joysticks();
+        // input handling (end)
+
         simulate();
 
         render();
@@ -60,8 +78,8 @@ int main(int argc, char* argv[]) {
         printf("%i\n", frameLength);
     }
 
-    if (joystick) {
-        SDL_CloseJoystick(joystick);
+    if (gamepad) {
+        SDL_CloseGamepad(gamepad);
     }
 
 }
@@ -71,7 +89,7 @@ int initEverything(){
 
     SDL_SetAppMetadata("Example Input Joystick Polling", "1.0", "com.example.input-joystick-polling");
 
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK)) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMEPAD)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -117,137 +135,31 @@ void render(){
     SDL_RenderPresent(renderer);
 }
 
-int handleEvents(){
-    SDL_Event event;
-    while(SDL_PollEvent(&event)){
-        if (event.type == SDL_EVENT_QUIT) {
-            return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-        } else if (event.type == SDL_EVENT_JOYSTICK_ADDED) {
-            /* this event is sent for each hotplugged stick, but also each already-connected joystick during SDL_Init(). */
-            if (joystick == NULL) {  /* we don't have a stick yet and one was added, open it! */
-                joystick = SDL_OpenJoystick(event.jdevice.which);
-                if (!joystick) {
-                    SDL_Log("Failed to open joystick ID %u: %s", (unsigned int) event.jdevice.which, SDL_GetError());
-                }
-            }
-        } else if (event.type == SDL_EVENT_JOYSTICK_REMOVED) {
-            if (joystick && (SDL_GetJoystickID(joystick) == event.jdevice.which)) {
-                SDL_CloseJoystick(joystick);  /* our joystick was unplugged. */
-                joystick = NULL;
-            }
-        }
-        return SDL_APP_CONTINUE;  /* carry on with the program! */    
+
+
+void print_joysticks(){
+    if (!gamepad) {  /* we have a stick opened? */
+        return;
     }
+
+    //const float size = 30.0f;
+    //int total;
+
+    /*
+    int total = SDL_GetNumJoystickAxes(joystick);
+    for (int i = 0; i < total; i++) {
+        const float val = (((float) SDL_GetJoystickAxis(joystick, i)) / 32767.0f);
+        printf("Axis%i: %f\n", i, val); 
+    }
+    */
+    Sint16 leftStickX = SDL_GetGamepadAxis(gamepad, SDL_GAMEPAD_AXIS_LEFTX);
+    Sint16 leftStickY = SDL_GetGamepadAxis(gamepad, SDL_GAMEPAD_AXIS_LEFTY);
+    printf("leftStickX: %i, leftStickY: %i\n", leftStickX, leftStickY);
+
+    Sint16 rightStickX = SDL_GetGamepadAxis(gamepad, SDL_GAMEPAD_AXIS_RIGHTX);
+    Sint16 rightStickY = SDL_GetGamepadAxis(gamepad, SDL_GAMEPAD_AXIS_RIGHTY);
+    printf("rightStickX: %i, rightStickY: %i\n", rightStickX, rightStickY);
+
 }
 
 
-
-
-
-/**
-This function is just to keep the demo code around so I can refer to it later
-when I want to start capturing controller input.
-It includes event capture and rendering in the same function, so I don't want
-to use it.
-*/
-void demo_example(){
-    int winw = 640, winh = 480;
-    const char *text = "Plug in a joystick, please.";
-    float x, y;
-    int i;
-
-    if (joystick) {  /* we have a stick opened? */
-        text = SDL_GetJoystickName(joystick);
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_GetWindowSize(window, &winw, &winh);
-
-    /* note that you can get input as events, instead of polling, which is
-       better since it won't miss button presses if the system is lagging,
-       but often times checking the current state per-frame is good enough,
-       and maybe better if you'd rather _drop_ inputs due to lag. */
-
-    if (joystick) {  /* we have a stick opened? */
-        const float size = 30.0f;
-        int total;
-
-        /* draw axes as bars going across middle of screen. We don't know if it's an X or Y or whatever axis, so we can't do more than this. */
-        total = SDL_GetNumJoystickAxes(joystick);
-        y = (float) ((winh - (total * size)) / 2);
-        x = ((float) winw) / 2.0f;
-        for (i = 0; i < total; i++) {
-            const SDL_Color *color = &colors[i % SDL_arraysize(colors)];
-            const float val = (((float) SDL_GetJoystickAxis(joystick, i)) / 32767.0f);  /* make it -1.0f to 1.0f */
-            const float dx = x + (val * x);
-            const SDL_FRect dst = { dx, y, x - SDL_fabsf(dx), size };
-            SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a);
-            SDL_RenderFillRect(renderer, &dst);
-            y += size;
-        }
-
-        /* draw buttons as blocks across top of window. We only know the button numbers, but not where they are on the device. */
-        total = SDL_GetNumJoystickButtons(joystick);
-        x = (float) ((winw - (total * size)) / 2);
-        for (i = 0; i < total; i++) {
-            const SDL_Color *color = &colors[i % SDL_arraysize(colors)];
-            const SDL_FRect dst = { x, 0.0f, size, size };
-            if (SDL_GetJoystickButton(joystick, i)) {
-                SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a);
-            } else {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            }
-            SDL_RenderFillRect(renderer, &dst);
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, color->a);
-            SDL_RenderRect(renderer, &dst);  /* outline it */
-            x += size;
-        }
-
-        /* draw hats across the bottom of the screen. */
-        total = SDL_GetNumJoystickHats(joystick);
-        x = ((float) ((winw - (total * (size * 2.0f))) / 2.0f)) + (size / 2.0f);
-        y = ((float) winh) - size;
-        for (i = 0; i < total; i++) {
-            const SDL_Color *color = &colors[i % SDL_arraysize(colors)];
-            const float thirdsize = size / 3.0f;
-            const SDL_FRect cross[] = { { x, y + thirdsize, size, thirdsize }, { x + thirdsize, y, thirdsize, size } };
-            const Uint8 hat = SDL_GetJoystickHat(joystick, i);
-
-            SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
-            SDL_RenderFillRects(renderer, cross, SDL_arraysize(cross));
-
-            SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a);
-
-            if (hat & SDL_HAT_UP) {
-                const SDL_FRect dst = { x + thirdsize, y, thirdsize, thirdsize };
-                SDL_RenderFillRect(renderer, &dst);
-            }
-
-            if (hat & SDL_HAT_RIGHT) {
-                const SDL_FRect dst = { x + (thirdsize * 2), y + thirdsize, thirdsize, thirdsize };
-                SDL_RenderFillRect(renderer, &dst);
-            }
-
-            if (hat & SDL_HAT_DOWN) {
-                const SDL_FRect dst = { x + thirdsize, y + (thirdsize * 2), thirdsize, thirdsize };
-                SDL_RenderFillRect(renderer, &dst);
-            }
-
-            if (hat & SDL_HAT_LEFT) {
-                const SDL_FRect dst = { x, y + thirdsize, thirdsize, thirdsize };
-                SDL_RenderFillRect(renderer, &dst);
-            }
-
-            x += size * 2;
-        }
-    }
-
-    x = (((float) winw) - (SDL_strlen(text) * SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE)) / 2.0f;
-    y = (((float) winh) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2.0f;
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, text);
-    SDL_RenderPresent(renderer);
-
-
-}
